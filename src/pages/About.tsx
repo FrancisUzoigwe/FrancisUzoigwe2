62fe3cbea7a00f1d093b66c525dcaa4599fc4ef2
FrancisUzoigwe <kossyuzoigwe@gmail.com>
2024-03-25 23:30:03 +0100

    react-swift-reveal

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,7 @@
 import front from "../assets/front.jpg"
 import ui from "../assets/ui.jpg"
 import backend from "../assets/backend.jpg"
+import { Zoom } from "react-swift-reveal"
 const About = () => {
     return (
         <div className="w-full min-h-[70vh] flex flex-col items-center bg-[#f0f3f5]" id="about">
@@ -16,21 +17,28 @@ const About = () => {
             <div className="my-2" />
             <div className="w-[90%] my-2 text-[20px] font-bold max-md:text-[16px]">Skills:</div>
             <div className="w-[90%] max-md:text-[15px] max-md:grid-cols-2 grid grid-cols-3 gap-4 max-md:gap-2">
-                <div className="min-h-[300px] bg-white rounded-xl flex flex-col items-center max-sm:col-span-3">
-                    <img className="w-[95%] h-[160px] my-2 border  rounded-xl object-cover" src={front} />
-                    <div className="font-black">Frontend Developement</div>
-                    <div className="w-[95%] text-[15px]">Proficient at creating, responsive high-end designs using React.js.</div>
-                </div>
-                <div className="h-[300px]  bg-white rounded-xl flex flex-col items-center max-sm:col-span-3">
-                    <img className="w-[95%] h-[160px] my-2 border  rounded-xl object-cover" src={backend} />
-                    <div className="font-black">Backend Developement</div>
-                    <div className="w-[95%] text-[15px]">Proficient at creating functional API's and designing scalabe database using MongoDB & MySQL.</div> </div>
-                <div className="h-[300px] max-md:col-span-3 bg-white rounded-xl flex flex-col items-center">
-                    <img className="w-[95%] h-[160px] my-2 border  rounded-xl object-cover" src={ui} />
-                    <div className="font-black">Ui/Ux Development</div>
-                    <div className="w-[95%] text-[15px]">Proficient at creating user appealing interfaces for web applications.
+                <Zoom>
+                    <div className="min-h-[300px] bg-white rounded-xl flex flex-col items-center max-sm:col-span-3">
+                        <img className="w-[95%] h-[160px] my-2 border  rounded-xl object-cover" src={front} />
+                        <div className="font-black">Frontend Developement</div>
+                        <div className="w-[95%] text-[15px]">Proficient at creating, responsive high-end designs using React.js.</div>
                     </div>
-                </div>
+                </Zoom>
+                <Zoom>
+                    <div className="h-[300px]  bg-white rounded-xl flex flex-col items-center max-sm:col-span-3">
+                        <img className="w-[95%] h-[160px] my-2 border  rounded-xl object-cover" src={backend} />
+                        <div className="font-black">Backend Developement</div>
+                        <div className="w-[95%] text-[15px]">Proficient at creating functional API's and designing scalabe database using MongoDB & MySQL.</div> </div>
+                </Zoom>
+                <Zoom>
+
+                    <div className="h-[300px] max-md:col-span-3 bg-white rounded-xl flex flex-col items-center">
+                        <img className="w-[95%] h-[160px] my-2 border  rounded-xl object-cover" src={ui} />
+                        <div className="font-black">Ui/Ux Development</div>
+                        <div className="w-[95%] text-[15px]">Proficient at creating user appealing interfaces for web applications.
+                        </div>
+                    </div>
+                </Zoom>
             </div>
         </div>
     )
